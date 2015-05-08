20064a870232e3875af16c8751d5752fa98d8a69
arnold <arnold@4525493e-7705-40b1-a816-d608a930855b>
2015-05-08 13:40:00 +0000

    Reorganized tmReporter and tmVhdlProducer drafts structure to a more pythonic way. Added tmEditor draft. Adjusted environmental setup.
    git-svn-id: https://svn.cern.ch/reps/cactus/trunk/cactusprojects/utm/tmEditor@36119 4525493e-7705-40b1-a816-d608a930855b

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Repository path   : $HeadURL: $
+# Last committed    : $Revision: $
+# Last changed by   : $Author: $
+# Last changed date : $Date: $
+#
+
+from distutils.core import setup
+from glob import glob
+
+setup(
+    name = 'tmEditor',
+    version = '0.1.0',
+    description = "Trigger Menu Editor for uGT upgrade",
+    author = "Bernhard Arnold",
+    author_email = "bernhard.arnold@cern.ch",
+    url = "https://twiki.cern.ch/twiki/bin/viewauth/CMS/GlobalTriggerUpgradeL1T-uTme",
+    packages = ['tmEditor', ],
+    data_files = [],
+    scripts = [
+        'scripts/tm-editor',
+    ],
+    provides = ['tmEditor', ],
+)
---
+++ tmEditor/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Repository path   : $HeadURL:  $
+# Last committed    : $Revision:  $
+# Last changed by   : $Author:  $
+# Last changed date : $Date: $
+#
+
