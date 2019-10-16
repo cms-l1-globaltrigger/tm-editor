8776a00fb61cb04c877309788913672e2ec6389b
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-10-16 19:01:43 +0200

    fixed tm-python dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url = "http://globaltrigger.hephy.at/upgrade/tme",
     packages = find_packages(),
     install_requires=[
-        'tm-python @ https://github.com/cms-l1-globaltrigger/tm-python.git@0.7.3',
+        'tm-python @ git+https://github.com/cms-l1-globaltrigger/tm-python@0.7.3',
         'Markdown>=3.1',
         'PyQt5>=5.13'
     ],
