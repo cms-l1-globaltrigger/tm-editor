81d0fe40c047ff0b6df4bc92c020bdce07cbac82
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-01-08 14:57:50 +0100

    using utm 0.7.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name='tm-editor',
-    version='0.11.0',
+    version='0.11.1',
     description="CMS Level-1 Trigger Menu Editor",
     long_description=long_description,
     author = "Bernhard Arnold",
