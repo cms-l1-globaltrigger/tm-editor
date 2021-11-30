d5f7925cad502329fd1655ec241f6c3a97905ca8
Bernhard Arnold <bernhard.arnold@cern.ch>
2021-11-30 14:59:30 +0100

    bump version

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Install using pip (>= 19.0)
 
 ```bash
 pip install --upgrade pip
-pip install git+https://github.com/cms-l1-globaltrigger/tm-editor.git@0.13.0
+pip install git+https://github.com/cms-l1-globaltrigger/tm-editor.git@0.14.0
 ```
 
 ## Build
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name='tm-editor',
-    version='0.13.0',
+    version='0.14.0',
     description="CMS Level-1 Trigger Menu Editor",
     long_description=long_description,
     author = "Bernhard Arnold",
